d621c3869dbd0ff9df1fd6eb6f2c8e6a7912dd5a
vivek.tiwari <vivek.tiwari@Vivek-Tiwari1.diasparkindia.com>
2018-12-03 22:31:37 +0530

    verifyheader

--- Sept15/src/main/java/com/Diaspark/app/homePage.java
+++ Sept15/src/main/java/com/Diaspark/app/homePage.java
@@ -8,7 +8,7 @@ public class homePage {
 
 	public void toVerifyHeaderContent(WebDriver driver) {
 
-		String arr[] = {"Home", "About Us", "Courses", "Corporate Training", "Contact Us" };
+		String arr[] = { "HOME", "ABOUT US", "COURSES", "CORPORATE TRAINING", "CONTACT US" };
 
 		int size = driver.findElements(By.xpath("//nav/ul/li/a/strong")).size();
 		System.out.println(size);
@@ -16,12 +16,14 @@ public class homePage {
 		for (int i = 1; i <= size; i++) {
 			String text = driver.findElement(By.xpath("(//nav/ul/li/a/strong)[" + i + "]")).getText();
 			System.out.println(text);
-
-			/*if (arr[i].equalsIgnoreCase(text)) {
+			System.out.println(arr[i-1]);
+			
+			int var = arr[i-1].compareTo(text);
+			if (var == 0) {
 				System.out.println("Pass");
 			} else {
 				System.out.println("Fail");
-			}*/
+			}
 		}
 
 	}
